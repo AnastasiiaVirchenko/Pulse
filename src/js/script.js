a54ea07e7b42244d1f7e62c1f0e95eeb839b55be
Tasia <tasia.virchenko@gmail.com>
2023-01-09 10:33:48 +0000

    mask input

--- src/js/script.js
+++ src/js/script.js
@@ -88,5 +88,25 @@ $(this).on('click', function () {
   valideForms('#order form');
 
   $('input[name=phone]').mask("+44 999-999-9999");
+  $('form').submit(function(e){
+    e.preventDefault();
+    if(!$(this).valid()) {
+      return;
+    }
+
+    $.ajax({
+      type: "POST",
+      url: "mailer/smart.php",
+      data: $(this).serialize()
+
+    }).done(function() {
+      $(this).find("input").val("");
+      $('#consultation, #order').fadeOut();
+      $('.overlay, #thanks').fadeIn('slow');
+      $('form').trigger('reset');
+  });
+  return false;
+  });
+
 
 });        
