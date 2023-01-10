42c468726098e3582610536deb48a71dc6541c53
Tasia <tasia.virchenko@gmail.com>
2023-01-10 11:59:20 +0000

    scroll

--- src/js/script.js
+++ src/js/script.js
@@ -108,5 +108,20 @@ $(this).on('click', function () {
   return false;
   });
 
+  //Smooth scroll and pageup
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 1600) {
+      $('.pageup').fadeIn();
+
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+  $("a[href^='#']").click(function(){
+    const _href = $(this).attr("href");
+    $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+    return false;
+  });
 
 });        
